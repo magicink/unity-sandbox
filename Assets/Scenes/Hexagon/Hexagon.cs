2327b5160fabf7d8e2b1b1bf12d9b1b385dfd5d6
Brandon Tom <magicink@gmail.com>
2021-04-25 11:53:09 -0700

    Clean-up

--- Assets/Scenes/Hexagon/Hexagon.cs
+++ Assets/Scenes/Hexagon/Hexagon.cs
@@ -1,32 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Draws a hexagon
-/// </summary>
-public class Hexagon : MonoBehaviour
+namespace Scenes.Hexagon
 {
-    [SerializeField] private GameObject cornerPrefab;
-    
-    private Vector2 center;
-    private float size = 1f;
-    private Vector2[] points = new Vector2[6];
-    private GameObject[] corners = new GameObject[6];
-
-    private void Start()
+    /// <summary>
+    /// Draws a hexagon
+    /// </summary>
+    public class Hexagon : MonoBehaviour
     {
-        center = transform.position;
-        for (var i = 0; i < points.Length; i++)
+        [SerializeField] private GameObject cornerPrefab;
+        [SerializeField] private float size = 1f;
+    
+        private Vector2 _center;
+        private readonly Vector2[] _points = new Vector2[6];
+        private readonly GameObject[] _corners = new GameObject[6];
+
+        private void Start()
         {
-            var angleDeg = 60 * i - 30;
-            var angleRad = Mathf.PI / 180 * angleDeg;
-            points[i] = new Vector2((center.x + size) * Mathf.Cos(angleRad), (center.y + size) * Mathf.Sin(angleRad));
-            if (cornerPrefab)
+            _center = transform.position;
+            for (var i = 0; i < _points.Length; i++)
             {
-                var corner = Instantiate(cornerPrefab, new Vector3(points[i].x, 0, points[i].y), Quaternion.identity);
-                corners[i] = corner;
+                var angleDeg = 60 * i - 30;
+                var angleRad = Mathf.PI / 180 * angleDeg;
+                _points[i] = new Vector2((_center.x + size) * Mathf.Cos(angleRad), (_center.y + size) * Mathf.Sin(angleRad));
+                if (!cornerPrefab) continue;
+                var corner = Instantiate(cornerPrefab, new Vector3(_points[i].x, 0, _points[i].y), Quaternion.identity);
+                _corners[i] = corner;
                 corner.name = i.ToString();
                 corner.transform.parent = transform;
                 var line = corner.AddComponent<LineRenderer>();
@@ -37,17 +35,14 @@ public class Hexagon : MonoBehaviour
 
                 if (i > 0)
                 {
-                    var lastPoint = points[i - 1];
-                    // Debug.Log($"Last point: {lastPoint}");
+                    var lastPoint = _points[i - 1];
                     line.SetPosition(1, new Vector3(lastPoint.x, 0, lastPoint.y));
                 }
 
-                if (i == 5)
-                {
-                    var firstCorner = corners[0];
-                    var firstLine = firstCorner.GetComponent<LineRenderer>();
-                    firstLine.SetPosition(1, corner.transform.position);
-                }
+                if (i != 5) continue;
+                var firstCorner = _corners[0];
+                var firstLine = firstCorner.GetComponent<LineRenderer>();
+                firstLine.SetPosition(1, corner.transform.position);
             }
         }
     }
